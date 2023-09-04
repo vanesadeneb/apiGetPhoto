c84865f44f5db5db7851917acd0dd59d30b8a91f
vanesavillarreal <vanessa_diesver@hotmail.com>
2023-09-04 12:16:39 -0700

    Adds query to get personal date and photo

---
+++ queries.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql');
+let { check_arguments, empty_result } = require('./validations');
+
+const connection = mysql.createConnection({
+    host: '192.168.105.131',
+    user: 'pruebas',
+    password: 'pruebas05',
+    database: 'licencias_luis'
+});
+
+connection.connect();
+
+const getFoto = (request, response) => {
+
+    try{
+
+        const { nombre, a_paterno, a_materno, fecha_nacimiento } = request.query;
+    
+        if (check_arguments( nombre, a_paterno, a_materno, fecha_nacimiento )){
+            
+            connection.query(`CALL GetPersonPhoto(?, ?, ?, ?)`,[nombre, a_paterno, a_materno, fecha_nacimiento],
+                (error, results) => {
+                    
+                    if (empty_result(results)) {
+                        response.status(400).json({ message: "Registro no encontrado"});
+                    } else {
+
+                        const jsonResults = JSON.parse(JSON.stringify(results[0]));
+                        const imagen = Buffer.from(jsonResults[0].IMAGEN).toString('base64');
+    
+                        if (jsonResults[0] && jsonResults[0].IMAGEN) {
+                            jsonResults[0].IMAGEN =`data:image/jpeg;base64,${imagen}`;
+                        }
+    
+                        response.status(200).json(jsonResults);
+                    }
+                    
+                }
+            ); 
+        }else {
+            response.status(400).json({
+                message: "Faltan parámetros requeridos"
+            });
+        }
+    
+    } catch ( error ) {
+        response.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
+
+module.exports = {
+  getFoto
+};
